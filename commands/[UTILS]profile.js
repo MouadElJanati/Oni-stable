0d5c11f80eb7f0adba9e532b63c3ea34b8ea6753
TSlash <supergamerzxtata@gmail.com>
2020-11-11 07:39:42 +0200

    added profile

--- commands/[UTILS]profile.js
+++ commands/[UTILS]profile.js
@@ -1,31 +1,20 @@
 const Discord = require("discord.js");
-const { Canvas } = require("canvas-constructor");
+const canvacord = require("canvacord");
 const fetch = require("node-fetch");
+const fs = require("fs");
 
 module.exports = {
   name: "profile",
   description: "Профиль пользователя.",
   async execute(client, message, args) {
-    const avatar = await fetch(message.author.avatarURL({ format: "jpg" }));
-
-    let mage = new Canvas(500, 250)
-      .setColor("#ffffff")
-      .addRect(0, 0, 500, 250) //we gonna replace it with image
-      .setColor("#ff2050")
-      .addRect(0, 0, 500, 80)
-      .setColor("#ffffff")
-      .setTextFont("bold 40px Impact") //you can make it bold
-      .addText("PROFILE CARD", 110, 55)
-      .setColor("#ff2050")
-      .setTextFont("bold 20px Impact")
-      .addText(`ID - ${message.author.id}`, 30, 140)
-      .addText(`TAG - ${message.author.tag}`, 30, 170)
-      .addText(`GUILD NAME - ${message.guild.name}`, 30, 200)
-      .setColor("#ffffff")
-      .addCircle(60, 40, 33)
-      .addCircularImage(avatar.buffer(), 60, 40, 30)
-      .toBuffer();
-
-    message.channel.send({ files: [mage] });
+    let image = await canvacord.rank({
+      username,
+      discrim,
+      avatarURL: message.author.displayAvatarURL({ format: "png" }),
+      color: "white",
+      background: "https://wallpaperaccess.com/full/45412.jpg",
+    });
+    let attachment = new Discord.MessageAttachment(image, "rank.png");
+    return message.channel.send(attachment);
   },
 };
